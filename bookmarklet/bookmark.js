e04ccb444e16f93f107dd9d2e3295ffeac5b1116
valhayot <valeriehayot@gmail.com>
2023-07-29 13:56:51 -0500

    Handle condition when bookmarklet is clicked multiple times and added leaf icon

--- bookmarklet/bookmark.js
+++ bookmarklet/bookmark.js
@@ -8,42 +8,51 @@
         var endpoint = segments.pop() || segments.pop();
 
         // step 2: query server for endpoint stats
-        let url = `https://129.114.109.109:5001/caws?endpoint=${endpoint}`
+        let url = `https://129.114.109.109:5000/caws?endpoint=${endpoint}`
         const response = await fetch(url);
         const endpoint_json = await response.json();
 
         // step 3: display results
-        // Create parent div
-        // TODO: handle case where div already exists
-        var energyDiv = document.createElement('div');
-        energyDiv.classList.add('caws');
-        energyDiv.classList.add('col-lg');
+        var parentEnergyElement = element.parentNode.parentNode.nextElementSibling.firstElementChild;
 
-        var energyRowDiv = document.createElement('div');
-        energyRowDiv.classList.add('row');
+        energyContentDiv = parentEnergyElement.querySelector('.caws-data');
 
-        // title div
-        var energyTitleDiv = document.createElement('div');
-        energyTitleDiv.classList.add('col-4');
-        energyTitleDiv.classList.add('col-lg-12');
-        energyTitleDiv.classList.add('small');
-        energyTitleDiv.classList.add('text-uppercase');
-        energyTitleDiv.classList.add('text-success');
-        energyTitleDiv.innerHTML = 'Total energy expenditure';
-        energyRowDiv.appendChild(energyTitleDiv);
+        if (energyContentDiv != null) {
+            // Edit value in existing div
+            energyContentDiv.innerHTML = endpoint_json.total_energy;
+        }
+        else {
+            // Create parent div
+            var energyDiv = document.createElement('div');
+            energyDiv.classList.add('caws');
+            energyDiv.classList.add('col-lg');
 
-        // content div
-        var energyContentDiv = document.createElement('div');
-        energyContentDiv.classList.add('col-8');
-        energyContentDiv.classList.add('col-lg-12');
-        energyContentDiv.classList.add('small');
-        energyContentDiv.innerHTML = endpoint_json.total_energy;
-        energyRowDiv.appendChild(energyContentDiv);
+            var energyRowDiv = document.createElement('div');
+            energyRowDiv.classList.add('row');
 
-        energyDiv.appendChild(energyRowDiv);
+            // title div
+            var energyTitleDiv = document.createElement('div');
+            energyTitleDiv.classList.add('col-4');
+            energyTitleDiv.classList.add('col-lg-12');
+            energyTitleDiv.classList.add('small');
+            energyTitleDiv.classList.add('text-uppercase');
+            energyTitleDiv.classList.add('text-success');
+            energyTitleDiv.innerHTML = 'Total energy expenditure (kWh) <img src="https://www.svgrepo.com/show/923/leaf.svg" alt="leaf" width="13px" heigh="13px" />';
+            energyRowDiv.appendChild(energyTitleDiv);
 
-        var elementSibling = element.parentNode.parentNode.nextElementSibling.firstElementChild.lastElementChild;
-        elementSibling.parentNode.insertBefore(energyDiv, elementSibling.nextSibling);
+            // content div
+            var energyContentDiv = document.createElement('div');
+            energyContentDiv.classList.add('caws-data')
+            energyContentDiv.classList.add('col-8');
+            energyContentDiv.classList.add('col-lg-12');
+            energyContentDiv.classList.add('small');
+            energyContentDiv.innerHTML = endpoint_json.total_energy;
+            energyRowDiv.appendChild(energyContentDiv);
+
+            energyDiv.appendChild(energyRowDiv);
+
+            parentEnergyElement.appendChild(energyDiv);
+        }
 
     }, endpoint_links);
 
