4648b429540cde6d836241dd8ed41f82d8761493
Alok Kamatar <alokvk2@uchicago.edu>
2023-08-08 12:52:47 -0500

    Adding scripts to deploy database

---
+++ services/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install and start docker
+yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+systemctl start docker
+
+# Start Postgres and pgadmin
+docker-compose up -d
+
+# Set firewall rules
+ufw allow 5432
+ufw allow 5050
